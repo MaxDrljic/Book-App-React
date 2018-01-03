68759de9d39e8d488c0185ac90b6a5c7cbc696c2
Max <max_spree@hotmail.com>
2018-01-03 16:47:45 +0100

    Connecting to Goodreads API & adding major changes

---
+++ src/components/pages/NewBookPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Segment  } from 'semantic-ui-react';
+import axios from 'axios';
+import SearchBookForm from '../forms/SearchBookForm';
+import BookForm from '../forms/BookForm';
+
+class NewBookPage extends React.Component {
+    state = {
+        book: null
+    };
+
+    onBookSelect = book => {
+        this.setState({ book });
+        axios
+            .get(`/api/books/fetchPages?goodreadsId=${book.goodreadsId}`)
+            .then(res => res.data.pages)
+            .then(pages => this.setState({ book: { ...book, pages }}));
+    };    
+
+    addBook = () => console.log('hi'); 
+  
+        render() {
+            return (
+                <Segment>
+                    <h1>Add new book to your collection</h1>
+                    <SearchBookForm onBookSelect={this.onBookSelect} />
+
+                    {this.state.book && (
+                        <BookForm submit={this.addBook} book={this.state.book} />
+                )}
+                </Segment>
+        );
+    }
+}
+
+export default NewBookPage;
